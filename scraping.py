59cf9e7faed97c046ef0eafb0461abe428258852
Crystin VanWagenen <44265061+CrystinVW@users.noreply.github.com>
2018-10-26 06:45:57 -0600

    Update and rename main.py to scraping.py

--- scraping.py
+++ scraping.py
@@ -10,6 +10,8 @@ def grabstats(game):
     for td_tag in soup.find_all('td'):
         stat = td_tag.text
         stats.append(stat)
+        
+# Insert the website in which you would like to scrap
 print(grabstats("http://www.espn.co.uk/nfl/matchup?gameId=400951612"))
 stats = [x.replace('\t', '').replace('\n', '') for x in stats]
 print(stats)
